3c3e63b729b5a1b25ad313ddb55be900947aa7ff
Njambiii <111266752+Njambiii@users.noreply.github.com>
2022-11-16 23:56:07 +0300

    Fixing bugs

--- memory.c
+++ memory.c
@@ -1,35 +1,18 @@
 #include "shell.h"
 
 /**
- * smart_alloc - allocate space if there is any, and add memory
- * to list of space to free
+ * bfree - frees a pointer and NULLs the address
+ * @ptr: address of the pointer to free
  *
- * @size: size of space to allocate
- *
- * Return: pointer to space in memory
+ * Return: 1 if freed, otherwise 0.
  */
-void *smart_alloc(size_t size)
+int bfree(void **ptr)
 {
-	void *ptr;
-
-	ptr = malloc(size);
-	if (ptr == NULL)
+	if (ptr && *ptr)
 	{
-		_puts("We used up all the memory!!! Holy shit\n");
-		exit(20);
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
 	}
-	return (ptr);
-}
-
-/**
- * free_array - frees a pointer array of strings
- *
- * @array: 2d array
- *
- * Return: void
- */
-void free_array(char **array)
-{
-	free(array[0]);
-	free(array);
+	return (0);
 }
